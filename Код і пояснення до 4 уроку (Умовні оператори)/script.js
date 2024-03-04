bcf65e2c37b58cff7f427dd439954bd927cec160
Dmytro <dimalimaredmi@gmail.com>
2024-03-04 09:39:44 +0200

    operator 4 lesson

---
+++ Код і пояснення до 4 уроку (Умовні оператори)/script.js
@@ -0,0 +1,41 @@
+// if Else oparator
+
+// const isUserLoggedIn = true;
+// if (isUserLoggedIn) {
+//   alert("You are logged in!");
+// } else {
+//   alert("You are not logged in!");
+// }
+
+// Tearnary Operator
+
+// const isUserLoggedIn = true;
+// isUserLoggedIn ? alert("You are logged in!") : alert("You are not logged in!");
+
+// Switch operator
+
+// const age = 30;
+// switch (age) {
+//   case 10:
+//     alert("Happy birthday, give me a kiss");
+//     break;
+//   case 20:
+//     alert("Happy, birthday, give me a hug");
+//   case 30:
+//     alert("Happy, birthday, give me a grandchildren");
+//   default:
+//     alert("Happy birthday, give me your legacy!");
+// }
+
+// const name = Vitaly;
+// switch (name) {
+//   case "Vitaly":
+//     alert("You are the best man");
+//     break;
+//   case "Andrew":
+//     alert("Sounds so-so");
+//   case "Igor":
+//     alert("Better would be Vitaly");
+//   default:
+//     alert("Vitaly is always the best");
+// }
